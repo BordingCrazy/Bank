1ef80b908b96a96497dd68bdef1ea3f002621919
Test <d.thor94@hotmail.com>
2020-10-07 04:40:34 +0000

    adding getters and setters.

--- src/ChallengeOne/Main.java
+++ src/ChallengeOne/Main.java
@@ -3,6 +3,6 @@ package ChallengeOne;
 public class Main {
 
     public static void main(String[] args) {
-        //testing testing
+
     }
 }
--- src/ChallengeOne/bankAccount.java
+++ src/ChallengeOne/bankAccount.java
@@ -1,5 +1,46 @@
 package ChallengeOne;
 
 public class bankAccount {
-    //testing
+    private int AccountNumber;
+    private double balance;
+    private string customerName;
+    private string email;
+    private string phoneNumber;
+
+    public int getAccountNumber() {
+        return this.AccountNumber;
+    }
+    public double getBalance() {
+        return this.balance;
+    }
+    public string getCustomerName() {
+        return this.customerName;
+    }
+    public string getEmail() {
+        return this.email;
+    }
+    public string getPhoneNumber() {
+        return this.phoneNumber;
+    }
+    public void setAccountNumber(int accountNumber) {
+        this.AccountNumber = accountNumber;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    public void setCustomerName(string customerName) {
+        this.customerName = customerName;
+    }
+    public void setEmail(string email) {
+        this.email = email;
+    }
+    public void setPhoneNumber(string phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public static void incrementBalance() {
+
+    }
+    public static void decrementBalance() {
+        
+    }
 }
